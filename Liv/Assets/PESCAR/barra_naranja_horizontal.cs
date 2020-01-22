44f22a58d1c22d88419385e8bccada87c930d4b3
Marcosy2000 <59959930+Marcosy2000@users.noreply.github.com>
2020-01-22 13:16:58 +0100

    Montar mapa 10

---
+++ Liv/Assets/PESCAR/barra_naranja_horizontal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barra_naranja_horizontal : MonoBehaviour
+{
+    float velocidad = 700.0f;
+
+    void Update()
+    {
+        float traslacionx = velocidad;
+        traslacionx = traslacionx * Time.deltaTime;
+        transform.Translate(traslacionx, 0, 0);
+
+
+
+        if (transform.position.x >= 1057.0f)
+        {
+            velocidad = -700.0f;
+        }
+        else if (transform.position.x <= 477.5f)
+        {
+
+            velocidad = 700.0f;
+        }
+
+
+
+    }
+}
---
+++ Liv/Assets/PESCAR/barra_verde_horizontal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barra_verde_horizontal : MonoBehaviour
+{
+    float velocidad = 400.0f;
+
+    void Update()
+    {
+        float traslacionx = velocidad;
+        traslacionx = traslacionx * Time.deltaTime;
+        transform.Translate(traslacionx, 0, 0);
+
+
+
+        if (transform.position.x >= 1020.0f)
+        {
+            velocidad = -400.0f;
+        }
+        else if (transform.position.x <= 512.5f)
+        {
+
+            velocidad = 400.0f;
+        }
+
+
+
+    }
+}
