9fcf09f745978a3bf44002df649baab039799a68
Marcosy2000 <59959930+Marcosy2000@users.noreply.github.com>
2020-01-23 11:59:38 +0100

    Escena con scripts

--- Liv/Assets/Scripts/controls/CameraFollow.cs
+++ Liv/Assets/Scripts/controls/CameraFollow.cs
@@ -4,13 +4,13 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
 
-    public float smoothSpeed=5f;
+    public float smoothSpeed = 5f;
     public Vector3 offset;
 
     void Update()
     {
         Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition,smoothSpeed*Time.deltaTime);
         transform.position = smoothedPosition;
     }
 }
